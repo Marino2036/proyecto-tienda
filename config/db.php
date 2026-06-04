a6dafa44167560fe0414771e52e417166ff5fb9a
Marino Bautista Perez <bpmarbautista50@gmail.com>
2026-06-03 20:29:31 -0600

    Preparar proyecto para Render

--- config/db.php
+++ config/db.php
@@ -1,15 +1,22 @@
 <?php
-$host = "127.0.0.1";
-$usuario = "root";
-$contrasena = "root";
-$basedatos = "tienda";
-$puerto = 3306;
 
-$conexion = new mysqli($host, $usuario, $contrasena, $basedatos, $puerto);
+$host = getenv("DB_HOST") ?: "127.0.0.1";
+$usuario = getenv("DB_USER") ?: "root";
+$contrasena = getenv("DB_PASS") ?: "root";
+$basedatos = getenv("DB_NAME") ?: "tienda";
+$puerto = getenv("DB_PORT") ?: 3306;
+
+$conexion = new mysqli(
+    $host,
+    $usuario,
+    $contrasena,
+    $basedatos,
+    $puerto
+);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-$conexion->set_charset("utf8");
+$conexion->set_charset("utf8mb4");
 ?>
